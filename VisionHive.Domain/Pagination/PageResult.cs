094b7917446697dfb3f782b40847db720a07e6b6
larissa557197 <larissampmuniz@gmail.com>
2025-09-21 20:08:56 -0300

    feat(domain): adicionar PageResult<T> para suporte à paginação

---
+++ VisionHive.Domain/Pagination/PageResult.cs
@@ -0,0 +1,14 @@
+﻿namespace VisionHive.Domain.Pagination;
+
+public class PageResult<T>
+{
+    public IEnumerable<T> Items { get; set; } =  new List<T>();
+    
+    public int Total {get; set;}
+    
+    public bool HasMore {get; set;}
+    
+    public int Page {get; set;}
+    
+    public int PageSize {get; set;}
+}
