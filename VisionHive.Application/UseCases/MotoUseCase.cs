0c7ed5ecd05e86c78442dce2e1055eb86d8b284e
larissa557197 <larissampmuniz@gmail.com>
2025-09-22 17:36:10 -0300

    feat(application): implementar MotoUseCase e IMotoUseCase para encapsular regras da entidade Moto

---
+++ VisionHive.Application/UseCases/MotoUseCase.cs
@@ -0,0 +1,26 @@
+﻿
+using VisionHive.Domain.Entities;
+using VisionHive.Domain.Pagination;
+using VisionHive.Infrastructure.Repositories;
+
+namespace VisionHive.Application.UseCases;
+
+public class MotoUseCase : IMotoUseCase
+{
+    private readonly IMotoRepository _motoRepository;
+
+    public MotoUseCase(IMotoRepository motoRepository)
+        => _motoRepository = motoRepository;
+
+    public Task<PageResult<Moto>> GetPaginationAsyncMoto(
+        int page, int pageSize, CancellationToken ct = default)
+        => _motoRepository.GetPaginationAsyncMoto(page, pageSize, ct);
+
+    public async Task<Moto> CreateAsync(Moto moto, CancellationToken ct = default)
+    {
+        await _motoRepository.AddAsync(moto, ct);
+        await _motoRepository.SaveChangesAsync(ct);
+        return moto;
+    }
+}
+    
