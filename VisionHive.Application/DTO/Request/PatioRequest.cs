fb072a09770449c68affcf0681717d498a494e95
larissa557197 <larissampmuniz@gmail.com>
2025-09-22 18:14:13 -0300

    refactor(dto): adicionar método ToDomain em PatioRequest

--- VisionHive.Application/DTO/Request/PatioRequest.cs
+++ VisionHive.Application/DTO/Request/PatioRequest.cs
@@ -1,9 +1,20 @@
-﻿namespace VisionHive.Application.DTO.Request
+﻿using VisionHive.Domain.Entities;
+
+namespace VisionHive.Application.DTO.Request
 {
     public class PatioRequest
     {
         public string Nome { get; set; }
         public int LimiteMotos { get; set; }
         public Guid FilialId { get; set; }
+
+        public Patio toDomain()
+        {
+            return new Patio(
+                nome: Nome,
+                limiteMotos: LimiteMotos,
+                filialId: FilialId
+            );
+        }
     }
 }
